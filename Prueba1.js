415f1ef24e4b2d27afc802b765ea93dc2d839b61
unknown <clurgo@mds-1163.sds_domain_1.local>
2013-10-03 19:59:21 -0300

    clase_1

--- Prueba1.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var panel_principal = $('#principal');
-    
-    var alta1 = new AltaDeUsuario();
-    panel_principal.append(alta1.ui);
-    
-    var alta2 = new AltaDeUsuario();
-    panel_principal.append(alta2.ui);
-});
